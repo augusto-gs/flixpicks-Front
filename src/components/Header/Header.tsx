2e2dba9e37c585cd0521278f6bde8be9d7b71e4c
Augusto Gomez <agomezsantos95@gmail.com>
2024-02-03 21:44:58 -0300

    Add logout logic and user interface element to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,43 @@
+import { useNavigate } from "react-router-dom";
+import useLocalStorage from "../../hooks/useLocalStorage/useLocalStorage";
+import { useAppDispatch } from "../../store/hooks";
 import Navigation from "../Navigation/Navigation";
 import HeaderStyled from "./HeaderStyled";
+import { userLogoutActionCreator } from "../../store/features/user/userSlice";
 
-const Header = (): React.ReactElement => (
-  <HeaderStyled className="main-header">
-    <img
-      className="main-header__logo"
-      src="/images/flixpicks_logo.svg"
-      alt="flixpicks logo on black letters"
-      width="197"
-      height="100"
-    />
-    <Navigation />
-  </HeaderStyled>
-);
+const Header = (): React.ReactElement => {
+  const { removeToken } = useLocalStorage();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleUserLogout = () => {
+    removeToken("FlixPicKsTokeN");
+    dispatch(userLogoutActionCreator());
+    navigate(`/login`);
+  };
+
+  return (
+    <HeaderStyled className="main-header">
+      <img
+        className="main-header__logo"
+        src="/images/flixpicks_logo.svg"
+        alt="flixpicks logo on black letters"
+        width="197"
+        height="100"
+      />
+      <button className="main-header__logout-button" onClick={handleUserLogout}>
+        <img
+          src="/images/logout_icon.svg"
+          alt="logout icon"
+          width="48"
+          height="48"
+          className="main-header__logout-icon"
+        />
+      </button>
+
+      <Navigation />
+    </HeaderStyled>
+  );
+};
 
 export default Header;
