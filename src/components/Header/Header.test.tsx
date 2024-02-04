fe60f2d0845769b67e75b3dbfdc63a644c67b209
Augusto Gomez <agomezsantos95@gmail.com>
2024-02-03 21:46:21 -0300

    Add testing suite for logout process

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -4,9 +4,19 @@ import Header from "./Header";
 import { customRenderWithBrowser } from "../../testUtils/testUtils";
 
 describe("Given a Header component", () => {
-  describe("When it is renderen on screen", () => {
+  describe("When it is rendered on screen", () => {
     test("Then it should show a text flixpicks logo on black letters 'flixpicks logo on black letters' on an alternative text", () => {
       const expectedAlternativeText = "flixpicks logo on black letters";
+
+      customRenderWithBrowser(<Header />);
+
+      const alternativeText = screen.getByAltText(expectedAlternativeText);
+
+      expect(alternativeText).toBeInTheDocument();
+    });
+
+    test("Then it should show an image with a 'logout icon' alternative text", () => {
+      const expectedAlternativeText = "logout icon";
 
       customRenderWithBrowser(<Header />);
 
